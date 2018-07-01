b41da84c8d75052e1c061465e3156467b36661b3
League <league@WTS8.attlocal.net>
2018-07-01 15:55:57 -0700

    progress

--- src/Hangman.java
+++ src/Hangman.java
@@ -15,25 +15,33 @@ public class Hangman {
 		int number = Integer.parseInt(numberString);
 		ArrayList<String> words = new ArrayList<String>();
 		ArrayList<Integer> indexes = new ArrayList<Integer>();
+		Hangman h = new Hangman();
 		Random r = new Random();
 		for (int i = 0; i < number; i++) {
-			indexes.add(r.nextInt(3000));
+			indexes.add(r.nextInt(2999));
 		}
+		h.sort(indexes);
 		int lineNum = 0;
+		int currentIndex = 0;
+//		System.out.println(c);
+//		for (int i = 0; i< indexes.size(); i++) {
+//			System.out.println(indexes.get(i));
+//		}
+		//System.out.println(indexes.get(0));
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("src/dictionary.txt"));
-			String TheLine;
-			for (int i: indexes) {
-				if (i == lineNum) {
-					for (int j = 0; j < i; j++) {
-						br.readLine();
+			String line = "";
+			while (line != null) {
+				for (int i = 0; i < indexes.size(); i++) {
+					if (indexes.get(currentIndex) == lineNum) {
+						currentIndex++;
+						line = br.readLine();
+						words.add(line);
 					}
-					TheLine = br.readLine();
-					words.add(TheLine);
 				}
-				//System.out.println(br.readLine());
 				lineNum++;
 			}
+		//	System.out.println(lineNum);
 			br.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -45,6 +53,20 @@ public class Hangman {
 		for (int i = 0; i < words.size(); i++) {
 			System.out.println(words.get(i));
 		}
-		System.out.println(words.size());
+		//System.out.println(words.size());
+	}
+	public void sort(ArrayList<Integer> indexes) {
+		boolean swap = true;
+		while (swap) {
+			swap = false;
+			for (int i = 0; i <indexes.size() - 1; i++) {
+				if (indexes.get(i) > indexes.get(i + 1)) {
+					int temp = indexes.get(i);
+					indexes.set(i, indexes.get(i+1));
+					indexes.set(i + 1, temp);
+					swap = true;
+				}
+			}
+		}
 	}
 }
