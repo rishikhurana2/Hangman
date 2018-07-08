446d31e1792432d61da89b16db14e5e0a327673e
League <league@WTS8.attlocal.net>
2018-07-08 15:55:42 -0700

    Done with Step 1 and Step 2

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -5,16 +7,21 @@ import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class Hangman {
+public class Hangman implements KeyListener {
 	public static void main(String[] args) throws IOException {
 	//	LineNumberReader lnr = new LineNumberReader(br);
 		String numberString = JOptionPane.showInputDialog("please input a number");
 		int number = Integer.parseInt(numberString);
 		ArrayList<String> words = new ArrayList<String>();
 		ArrayList<Integer> indexes = new ArrayList<Integer>();
+		Stack<String> wordsReciever = new Stack<String>();
 		Hangman h = new Hangman();
 		Random r = new Random();
 		for (int i = 0; i < number; i++) {
@@ -22,26 +29,18 @@ public class Hangman {
 		}
 		h.sort(indexes);
 		int lineNum = 0;
-		int currentIndex = 0;
-//		System.out.println(c);
-//		for (int i = 0; i< indexes.size(); i++) {
-//			System.out.println(indexes.get(i));
-//		}
-		//System.out.println(indexes.get(0));
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("src/dictionary.txt"));
 			String line = "";
 			while (line != null) {
+				line = br.readLine();
 				for (int i = 0; i < indexes.size(); i++) {
-					if (indexes.get(currentIndex) == lineNum) {
-						currentIndex++;
-						line = br.readLine();
+					if (indexes.get(i) == lineNum) {	
 						words.add(line);
 					}
 				}
 				lineNum++;
 			}
-		//	System.out.println(lineNum);
 			br.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -50,10 +49,11 @@ public class Hangman {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for (int i = 0; i < words.size(); i++) {
-			System.out.println(words.get(i));
-		}
-		//System.out.println(words.size());
+		h.putInStack(words, wordsReciever);
+		h.createUI();
+		String DisplayLine = wordsReciever.pop();
+		System.out.println(DisplayLine);
+		System.out.println(DisplayLine.substring(0, 1));
 	}
 	public void sort(ArrayList<Integer> indexes) {
 		boolean swap = true;
@@ -68,5 +68,36 @@ public class Hangman {
 				}
 			}
 		}
+	}
+	public void putInStack(ArrayList<String> words, Stack<String> wordsReciever) {
+		for (int i = 0; i< words.size(); i++) {
+			wordsReciever.push(words.get(i));
+		}
+	}
+	public void createUI() {
+		JFrame gameFrame = new JFrame();
+		JPanel gamePanel = new JPanel();
+		JLabel gameLabel = new JLabel();
+		gameFrame.setVisible(true);
+		gameFrame.setSize(1000, 500);
+		gameFrame.add(gamePanel);
+		gameLabel.setText("Guess a letter");
+		gamePanel.add(gameLabel);
+		gamePanel.addKeyListener(this);
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
