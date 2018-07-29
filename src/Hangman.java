c6f472efe03c6fac49cd0ad50a3431bb4c672da2
Rishi Khurana <league@WTS14-4.attlocal.net>
2018-07-29 15:57:46 -0700

    Done

--- src/Hangman.java
+++ src/Hangman.java
@@ -18,6 +18,13 @@ public class Hangman implements KeyListener {
 	static Hangman h = new Hangman();
 	static String underScores = " ";
 	static ArrayList<Character> underScoresArray = new ArrayList<Character>();
+	static Stack<String> wordsReciever = new Stack<String>();
+	static JLabel label = new JLabel();
+	JLabel livesCounter = new JLabel();
+	JFrame gameFrame = new JFrame();
+	static String set = "";
+	static String newSet = "";
+	int lives = 10;
 
 	public static void main(String[] args) throws IOException {
 		// LineNumberReader lnr = new LineNumberReader(br);
@@ -25,7 +32,6 @@ public class Hangman implements KeyListener {
 		int number = Integer.parseInt(numberString);
 		ArrayList<String> words = new ArrayList<String>();
 		ArrayList<Integer> indexes = new ArrayList<Integer>();
-		Stack<String> wordsReciever = new Stack<String>();
 		Random r = new Random();
 		for (int i = 0; i < number; i++) {
 			indexes.add(r.nextInt(2999));
@@ -55,6 +61,10 @@ public class Hangman implements KeyListener {
 		h.putInStack(words, wordsReciever);
 		DisplayLine = wordsReciever.pop();
 		System.out.println(DisplayLine);
+		for (int i = 0; i < DisplayLine.length(); i++) {
+			set += "_" + " ";
+		}
+		label.setText(set);
 		int length = DisplayLine.length();
 		for (int i = 0; i < length; i++) {
 			underScoresArray.add('_');
@@ -63,6 +73,7 @@ public class Hangman implements KeyListener {
 			underScores += underScoresArray.get(i) + " ";
 		}
 		h.createUI();
+		underScores = "";
 	}
 
 	public void sort(ArrayList<Integer> indexes) {
@@ -88,15 +99,14 @@ public class Hangman implements KeyListener {
 
 	public void createUI() {
 		JOptionPane.showMessageDialog(null, "Guess some words");
-		JFrame gameFrame = new JFrame();
 		JPanel textHolder = new JPanel();
-		JLabel label = new JLabel();
-		label.setText(underScores);
+		gameFrame.addKeyListener(this);
 		gameFrame.setVisible(true);
 		gameFrame.setSize(500, 250);
 		gameFrame.add(textHolder);
 		textHolder.add(label);
-		gameFrame.addKeyListener(this);
+		textHolder.add(livesCounter);
+
 	}
 
 	@Override
@@ -106,7 +116,44 @@ public class Hangman implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-
+		for (int i = 0; i < DisplayLine.length(); i++) {
+			if (e.getKeyChar() == DisplayLine.charAt(i)) {
+				underScoresArray.set(i, DisplayLine.charAt(i));
+			}
+		}
+		for (int i = 0; i < underScoresArray.size(); i++) {
+			underScores += underScoresArray.get(i) + " ";
+		}
+		String temp = e.getKeyChar() + "";
+		if (!DisplayLine.contains(temp)) {
+			lives--;
+		}
+		label.setText(underScores);
+		String temp2 = lives + "";
+		livesCounter.setText(temp2);
+		if (!underScoresArray.contains('_')) {
+			JOptionPane.showMessageDialog(null, "Game Over//Good job");
+			wordsReciever.remove(DisplayLine);
+			DisplayLine = wordsReciever.pop();
+			System.out.println(DisplayLine);
+			underScoresArray.clear();
+			newSet = "";
+			for (int i = 0; i < DisplayLine.length(); i++) {
+				newSet += "_" + " ";
+			}
+			label.setText(newSet);
+			for (int i = 0; i < DisplayLine.length(); i++) {
+				underScoresArray.add('_');
+			}
+			for (int i = 0; i < underScoresArray.size(); i++) {
+				underScores += underScoresArray.get(i) + " ";
+			}
+		}
+		if (lives <= 0) {
+			JOptionPane.showMessageDialog(null, "Game Over//You ran out of lives");
+			gameFrame.setVisible(false);
+		}
+		underScores = "";
 	}
 
 	@Override
